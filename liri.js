1fe02ca78cd247ac50382377e30041c069996e09
Mathew Bishop <39043451+MattyyB@users.noreply.github.com>
2018-10-15 15:12:33 -0500

    init files

---
+++ liri.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+
+var key = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
+
+// features/user input
+    // concert-this
+    // spotify-this-song
+    // movie-this
+    // do-what-it-says
+
+// bands in town "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+    // need name, venue, date of event MM/DD/YYYY
+ 
+// spotify     node spotify api https://www.npmjs.com/package/node-spotify-api
+    // need artist, song name, preview link, album
+
+// movie    OMDB    default to mr. nobody 
+    // Title of the movie.
+    // Year the movie came out.
+    // IMDB Rating of the movie.
+    // Rotten Tomatoes Rating of the movie.
+    // Country where the movie was produced.
+    // Language of the movie.
+    // Plot of the movie.
+    // Actors in the movie.
+
+// option 4   
+    // random.txt
+    // spotify for I Want it That Way
