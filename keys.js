f5a55a8e7da3edd6372c4c3adbd3cc538648ecae
Mathew Bishop <39043451+MattyyB@users.noreply.github.com>
2018-10-18 14:19:04 -0500

    spotify working

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 console.log('this is loaded');
 
-exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+exports.spotifyKeys = {
+  id: "0f347acc81a44a0ab1cf891d4b329b51",
+  secret: "a880977788ff4b239394e88ce11d9fda"
 };
--- liri.js
+++ liri.js
@@ -1,9 +1,10 @@
 require("dotenv").config();
 
-var key = require("./keys.js");
+var keys = require("./keys.js");
 var fs = require("fs");
 var request = require("request");
-var spotify = new Spotify(keys.spotify);
+var Spotify = require("node-spotify-api");
+
 
 var command = process.argv[2];
 var arg = process.argv[3];
@@ -14,11 +15,62 @@ var arg = process.argv[3];
     // movie-this
     // do-what-it-says
 
+    switch (command) {
+        case "concert-this":
+        bandsInTown(arg);
+        break;
+
+        case "spotify-this-song":
+        spotify(arg);
+        break;
+
+        case "movie-this":
+        movie(arg);
+        break;
+
+        case "do-what-it-says":
+        random();
+        break;
+    };
+
 // bands in town "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
     // need name, venue, date of event MM/DD/YYYY
+
+function bandsInTown(arg) {
+    let artist = arg;
+    let queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+    request(queryURL, function(error, body, response) {
+        if (arg == null) {
+            console.log("Please enter an artist next time.");
+        }
+        
+        if (!error && response.statusCode === 200) {
+            console.log(JSON.parse(body));
+            // console.log("Name: " + JSON.parse(body).artistname);
+        }
+    })
+}
  
 // spotify     node spotify api https://www.npmjs.com/package/node-spotify-api
     // need artist, song name, preview link, album
+
+function spotify(arg) {
+    let spotify = new Spotify(keys.spotifyKeys);
+
+    spotify.search({ type: "track", query: arg }, function(error, data) {
+        if (error) {
+            console.log("Error " + error);
+            return;
+        }
+
+        let songInfo = data.tracks.items;
+        console.log("Artist: " + songInfo[0].artists[0].name);
+        console.log("Song: " + songInfo[0].name);
+        console.log("Link " + songInfo[0].preview_url);
+        console.log("Album: " + songInfo[0].album.name);
+    })
+}
 
 // movie    OMDB    default to mr. nobody 
     // Title of the movie.
